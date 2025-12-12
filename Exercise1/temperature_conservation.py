b0e255504717f731f4c2cebbb64a30f15b6c6b02
Thomas Wotoro <didymusthomas6@gmail.com>
2025-12-12 21:09:51 +0300

    Update exercise 2 with assert functions

--- Exercise1/temperature_conservation.py
+++ Exercise1/temperature_conservation.py
@@ -40,10 +40,27 @@ if __name__ == "__main__":
     
 # Tests
 
-print(assert convertToCelsius(0) == -17.77777777777778)
+assert convertToCelsius(0) == -17.77777777777778 
 assert convertToCelsius(180) == 82.22222222222223 
 assert convertToFahrenheit(0) == 32 
 assert convertToFahrenheit(100) == 212 
 assert convertToCelsius(convertToFahrenheit(15)) == 15 
 
+# Rounding errors cause a slight discrepancy: 
+assert convertToCelsius(convertToFahrenheit(42)) == 42.00000000000001
+
+
+"""
+Solution Template
+
+def convertToFahrenheit(degreesCelsius): 
+# Calculate and return the degrees Fahrenheit: 
+    return ____ * (9 / 5) + 32 
+    
+def convertToCelsius(degreesFahrenheit): 
+# Calculate and return the degrees Celsius: 
+    return (____ - 32) * (____ / ____) 
+
+
+"""
 
