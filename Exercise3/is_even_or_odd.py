1cccbe648425151e40ca54affd8ea53ab7fa0af1
tom <didymusthomas6@gmail.com>
2026-01-13 22:45:40 +0300

    Update is_odd function

--- Exercise3/is_even_or_odd.py
+++ Exercise3/is_even_or_odd.py
@@ -3,8 +3,12 @@ def main():
     result = is_even(num)
     print(result)
 
+    res = is_odd(num)
+    print(res)
+
     # Tests
     assert is_even(4)
+    assert is_odd(7)
 
 
 def is_even(num):
@@ -15,8 +19,12 @@ def is_even(num):
     return result
 
 
-def is_odd(int: num):
-    ...
+def is_odd(num):
+    result = False
+    if num % 2 != 0:
+        result = True
+
+    return result
 
 
 if __name__ == "__main__":
